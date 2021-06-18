eb5d5f1926e646560e7780f3fdf9306e28bcd34b
saaketh <saakethaj@gmail.com>
2021-06-18 22:12:30 +0530

    YT added

--- README.md
+++ README.md
@@ -7,11 +7,14 @@ git clone https://github.com/Saakethjayarama/rf-netflix-clone
 cd rf-todos
 npm i
 npm start
+
 ```
 
 ## This project demonstrates the usage of
 
 1. React Hooks (useEffect, useState)
 2. TMDB API
+3. React Youtube
+4. Movie Trailer
 
 # Screenshots
--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_uri = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const [currentTrailerMovieId, setCurrentTrailerMovieId] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -19,6 +23,23 @@ function Row({ title, fetchURL, isLargeRow }) {
     })();
   }, [fetchURL]);
 
+  const handleClick = (movieId) => {
+    if (movieId == currentTrailerMovieId) {
+      setTrailerUrl("");
+      setCurrentTrailerMovieId(null);
+    } else {
+      setCurrentTrailerMovieId(movieId);
+      const selectedMovie = movies.filter((mv) => mv.id == movieId)[0];
+      console.log(selectedMovie);
+
+      movieTrailer(selectedMovie?.name || selectedMovie?.title || "")
+        .then((url) => {
+          setTrailerUrl(new URLSearchParams(new URL(url).search).get("v"));
+        })
+        .catch((err) => console.error(err));
+    }
+  };
+
   return (
     <div className="Row">
       <h2 className="Row__title">{title}</h2>
@@ -26,12 +47,19 @@ function Row({ title, fetchURL, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie.id)}
             src={`${base_uri}${movie.poster_path}`}
             alt={movie.name}
             className={isLargeRow ? `Row__image Large` : `Row__image`}
           />
         ))}
       </div>
+      {trailerUrl && (
+        <YouTube
+          videoId={trailerUrl}
+          opts={{ width: "100%", height: 390, playerVars: { autoplay: 1 } }}
+        />
+      )}
     </div>
   );
 }
